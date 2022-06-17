caefba611e95c8daf11422d9323df5f89b2587a4
Park-Beomjin <87348209+pbj97@users.noreply.github.com>
2022-06-17 14:51:08 +0900

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import dog from './images/dog.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="꿈은 이루어진다">
+        <img src={dog} alt="Dog"></img>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
