fa6672ec1afd0ce07d9ddcea38e1149fcdb8be98
Beomjin Park <pbj0556@jbnu.ac.kr>
2022-06-23 22:32:34 +0900

    수정

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
+import { Container, Box, Heading } from '@chakra-ui/react';
 const Page = () => {
-  return <div>Hello~</div>;
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        안녕하세요! 한국에 살고 있는 프론트엔드 개발자 박범진이라고 합니다.
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            박범진
+          </Heading>
+          <p>그냥 이것저것 잡다하게 하는 공돌이 인생</p>
+        </Box>
+      </Box>
+    </Container>
+  );
 };
 
 export default Page;
