7756ba7c20843716fcef52a1ee20160ec8b29701
Beomjin Park <pbj0556@jbnu.ac.kr>
2022-06-24 09:52:16 +0900

    이름수정

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -8,18 +8,18 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Takuya's homepage" />
-        <meta name="author" content="Takuya Matsuyama" />
-        <meta name="author" content="craftzdog" />
+        <meta name="description" content="Beomjin's homepage" />
+        <meta name="author" content="Park Beomjin" />
+        <meta name="author" content="beomjin" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta name="twitter:title" content="Takuya Matsuyama" />
+        <meta name="twitter:title" content="Park Beomjin" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
+        <meta name="twitter:site" content="@beomjin" />
+        <meta name="twitter:creator" content="@beomjin" />
         <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
-        <meta property="og:site_name" content="Takuya Matsuyama" />
-        <meta name="og:title" content="Takuya Matsuyama" />
+        <meta property="og:site_name" content="Park Beomjin" />
+        <meta name="og:title" content="Park Beomjin" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://www.craftz.dog/card.png" />
         <title>Parkbeomjin - Homepage</title>
